c9169b9d5903752ec9817a10a787f8d816b17fbc
itswhts4dinner <jmclaughl@gmail.com>
2020-03-11 14:16:24 -0400

    Create list_to_string.py

---
+++ anagram_solver/list_to_string.py
@@ -0,0 +1,7 @@
+str1 = " "  
+#here is a list or words
+jimbob = ['Jeremy', 'Michael', 'McLaughlin']
+# make a string our of the list 
+new_string=str1.join(jimbob)
+        
+print(new_string)       
